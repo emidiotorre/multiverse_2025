30e2bb21413acaaf64ae21827dbc3d7883a66858
Undo994 <alberto.demarino94@gmail.com>
2023-03-01 12:40:50 +0100

    fix button about mobile

--- multiverse-web/src/app/components/Header.tsx
+++ multiverse-web/src/app/components/Header.tsx
@@ -1,13 +1,13 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Container from './Container'
-import MenuSwitch from './MenuSwitch'
-import logoSmall from '../../../public/materiale/mvs_logo_small.png'
-import { usePathname } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Container from "./Container";
+import MenuSwitch from "./MenuSwitch";
+import logoSmall from "../../../public/materiale/mvs_logo_small.png";
+import { usePathname } from "next/navigation";
 function Header() {
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <>
       <div className="fixed bottom-0 w-full md:static md:top-0 text-xs md:text-base z-50 bg-white flex-col-reverse">
@@ -25,7 +25,7 @@ function Header() {
                     className="object-contain object-left md:object-cover max-h-6 w-20 md:w-auto md:max-w-max md:max-h-12"
                     src={logoSmall}
                     width={200}
-                    alt={''}
+                    alt={""}
                     priority={true}
                   />
                 </Link>
@@ -36,8 +36,8 @@ function Header() {
                   prefetch={true}
                   replace={true}
                   href="/about"
-                  className={`bg-[#d0d0d0] px-2 py-2  md:px-8 md:py-3  transition-colors  hover:z-10 hover:drop-shadow-lg rounded-full ${
-                    pathname?.split('/').includes('about') ? '' : ' -ml-12 z-0'
+                  className={`bg-[#d0d0d0] px-2 py-3 mr-3  md:px-8 md:py-3  transition-colors  hover:z-10 rounded-full ${
+                    pathname?.split("/").includes("about") ? "" : " -ml-12 z-0"
                   }`}
                 >
                   about
@@ -48,7 +48,7 @@ function Header() {
         </header>
       </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
