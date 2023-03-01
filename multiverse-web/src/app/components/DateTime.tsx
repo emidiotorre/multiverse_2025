8271c5e775ad52c014293fd1d36c2c3df6238091
Undo994 <alberto.demarino94@gmail.com>
2023-03-01 12:20:19 +0100

    fix footer

--- multiverse-web/src/app/components/DateTime.tsx
+++ multiverse-web/src/app/components/DateTime.tsx
@@ -1,40 +1,40 @@
-'use client'
-import { useEffect, useState } from 'react'
+"use client";
+import { useEffect, useState } from "react";
 
 const DateTime = () => {
-  var [date, setDate] = useState(new Date())
-  var [timerText, setTimerText] = useState('')
-  let str = '\u00B7'
+  var [date, setDate] = useState(new Date());
+  var [timerText, setTimerText] = useState("");
+  let str = "\u00B7";
   useEffect(() => {
-    var timer = setInterval(() => setDate(new Date()))
+    var timer = setInterval(() => setDate(new Date()));
     return function cleanup() {
-      clearInterval(timer)
-    }
-  })
+      clearInterval(timer);
+    };
+  });
   useEffect(() => {
     setTimerText(
       date.getUTCFullYear() +
-        ' ' +
-        (date.getUTCMonth() + 1).toString().padStart(2, '0') +
-        ' ' +
+        " " +
+        (date.getUTCMonth() + 1).toString().padStart(2, "0") +
+        " " +
         date.getUTCDate() +
-        ' ' +
+        " " +
         str +
-        ' ' +
-        (date.getUTCHours() + 1).toString().padStart(2, '0') +
-        ':' +
-        date.getUTCMinutes().toString().padStart(2, '0') +
-        ':' +
-        date.getUTCSeconds().toString().padStart(2, '0') +
-        ':' +
-        date.getUTCMilliseconds().toString().padStart(3, '0'),
-    )
-  }, [date])
+        " " +
+        (date.getUTCHours() + 1).toString().padStart(2, "0") +
+        ":" +
+        date.getUTCMinutes().toString().padStart(2, "0") +
+        ":" +
+        date.getUTCSeconds().toString().padStart(2, "0") +
+        ":" +
+        date.getUTCMilliseconds().toString().padStart(3, "0")
+    );
+  }, [date]);
   return (
     <div className="flex text-red-500 font-mono">
-      <small className="px-2">{timerText}</small>
+      <small className="px-0 md:px-2">{timerText}</small>
     </div>
-  )
-}
+  );
+};
 
-export default DateTime
+export default DateTime;
--- multiverse-web/src/app/components/FooterComponent.tsx
+++ multiverse-web/src/app/components/FooterComponent.tsx
@@ -1,24 +1,24 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Container from './Container'
-import DateTime from './DateTime'
-import Logo from '../../../public/materiale/mvs_logo.png'
-import { usePathname } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Container from "./Container";
+import DateTime from "./DateTime";
+import Logo from "../../../public/materiale/mvs_logo.png";
+import { usePathname } from "next/navigation";
 
 type Props = {
-  footer: any
-}
+  footer: any;
+};
 
 function FooterComponent({ footer }: Props) {
-  const pathName = usePathname()
+  const pathName = usePathname();
 
   return (
     <footer className="relative flex-1 max-w-full pt-10 md:top-24 flex align-bottom font-Sharp_Grotesk_Medium20 ">
-      <Container style={{ alignSelf: 'flex-end' }}>
-        <div className=" flex justify-between items-end py-10 ">
-          <div className="flex col-span-1 pt-0 ">
+      <Container style={{ alignSelf: "flex-end" }}>
+        <div className="md:flex md:justify-between md:items-end md:py-10 ">
+          <div className="md:col-span-1 pt-0 ">
             <small>
               <Link
                 href="https://instagram.com"
@@ -37,13 +37,13 @@ function FooterComponent({ footer }: Props) {
 
             <small key={footer.id}>{footer.col1}</small>
           </div>
-          <div className="flex flex-col-reverse ">
+          <div className="flex flex-col-reverse">
             <DateTime />
           </div>
         </div>
       </Container>
     </footer>
-  )
+  );
 }
 
-export default FooterComponent
+export default FooterComponent;
