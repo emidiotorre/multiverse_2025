b40f55f41c571bcc6d9c73ef73e259fa0cfe71ea
Undo994 <alberto.demarino94@gmail.com>
2023-04-03 19:55:38 +0200

    fix leading home

--- multiverse-web/src/app/page.tsx
+++ multiverse-web/src/app/page.tsx
@@ -1,17 +1,17 @@
-import { fetcher } from './apollo'
-import { GET_HOME } from './apollo/queries'
-import HomeComponent from './components/HomeComponent'
-import Image from 'next/image'
-import Logo from './components/Logo'
-import Container from './components/Container'
+import { fetcher } from "./apollo";
+import { GET_HOME } from "./apollo/queries";
+import HomeComponent from "./components/HomeComponent";
+import Image from "next/image";
+import Logo from "./components/Logo";
+import Container from "./components/Container";
 
 export default async function Home() {
-  const { home } = await fetcher({ query: GET_HOME, variables: null })
+  const { home } = await fetcher({ query: GET_HOME, variables: null });
   return (
     <>
       <Container>
         <div>
-          <h1 className="font-sans font-semibold w-full text-2xl md:text-5xl pb-20 break-words leading-[1.2] overflow-visible pt-4">
+          <h1 className="font-sans font-semibold w-full text-2xl md:text-5xl pb-20 break-words lg:leading-[1.1] overflow-visible pt-4">
             <div className="inline-flex self-baseline mr-2">
               <Logo height=".95em" width="100%" />
             </div>
@@ -21,5 +21,5 @@ export default async function Home() {
       </Container>
       <HomeComponent />
     </>
-  )
+  );
 }
