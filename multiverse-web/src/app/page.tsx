c5dd6f28b54cbde9fdbc78bb6b96c01c6a0f09e5
Undo994 <alberto.demarino94@gmail.com>
2023-04-04 18:24:47 +0200

    fix font home

--- multiverse-web/src/app/page.tsx
+++ multiverse-web/src/app/page.tsx
@@ -1,7 +1,6 @@
 import { fetcher } from "./apollo";
 import { GET_HOME } from "./apollo/queries";
 import HomeComponent from "./components/HomeComponent";
-import Image from "next/image";
 import Logo from "./components/Logo";
 import Container from "./components/Container";
 
@@ -11,7 +10,7 @@ export default async function Home() {
     <>
       <Container>
         <div>
-          <h1 className="font-sans font-semibold w-full text-2xl md:text-5xl pb-20 break-words lg:leading-[1.1] overflow-visible pt-4">
+          <h1 className="font-sans font-semibold w-full text-[1.8rem] md:text-5xl pb-20 break-words leading-[1.1] overflow-visible pt-4">
             <div className="inline-flex self-baseline mr-2">
               <Logo height=".95em" width="100%" />
             </div>
