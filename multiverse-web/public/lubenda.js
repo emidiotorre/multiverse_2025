92da178d197ebc8f7905dfd69335f963578bf640
Undo994 <alberto.demarino94@gmail.com>
2023-04-11 13:16:51 +0200

    cookie policy

---
+++ multiverse-web/public/lubenda.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-use-before-define
+var _iub = _iub || [];
+_iub.csConfiguration = {
+  askConsentAtCookiePolicyUpdate: true,
+  countryDetection: true,
+  enableLgpd: true,
+  enableUspr: true,
+  floatingPreferencesButtonDisplay: "bottom-right",
+  lang: "it",
+  lgpdAppliesGlobally: false,
+  perPurposeConsent: true,
+  siteId: 3034601,
+  whitelabel: false,
+  cookiePolicyId: 22712684,
+  banner: {
+    acceptButtonDisplay: true,
+    closeButtonDisplay: false,
+    customizeButtonDisplay: true,
+    explicitWithdrawal: true,
+    listPurposes: true,
+    position: "float-top-center",
+    rejectButtonDisplay: true,
+    showPurposesToggles: true,
+  },
+};
--- multiverse-web/src/app/layout.tsx
+++ multiverse-web/src/app/layout.tsx
@@ -21,6 +21,16 @@ export default async function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <script src="/lubenda.js" />
+      <script
+        type="text/javascript"
+        src="//cdn.iubenda.com/cs/gpp/stub.js"
+      ></script>
+      <script
+        type="text/javascript"
+        src="//cdn.iubenda.com/cs/iubenda_cs.js"
+        async
+      ></script>
       <body>
         <main className="relative pb-[5vh] pt-0 md:pb-0 flex flex-col w-full justify-start min-h-[100svh] ">
           <Header />
