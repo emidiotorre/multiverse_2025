dd03b18fcce39b6abac47bd39ca0f22e4c8e3138
Undo994 <alberto.demarino94@gmail.com>
2023-04-20 11:01:45 +0200

    fix margin-b to project

--- multiverse-web/src/app/components/Project.tsx
+++ multiverse-web/src/app/components/Project.tsx
@@ -148,7 +148,6 @@ const Project = ({ work }: Props) => {
                           borderRadius: '0.5rem',
                           overflow: 'hidden',
                           position: 'relative',
-                          marginBottom: '0.5rem',
                           objectFit: 'cover',
                           aspectRatio: '8/5',
                         }}
@@ -324,7 +323,7 @@ const Project = ({ work }: Props) => {
                       style={{
                         aspectRatio: img?.item.aspect_ratio,
                       }}
-                      className="row-span-2 mb-2 w-full h-full relative"
+                      className="row-span-2  w-full h-full relative"
                     >
                       <Image
                         fill={true}
@@ -367,7 +366,7 @@ const Project = ({ work }: Props) => {
                       style={{
                         aspectRatio: img?.item.aspect_ratio,
                       }}
-                      className="row-span-2 mb-2  w-full h-full relative "
+                      className="row-span-2   w-full h-full relative "
                     >
                       <Image
                         fill={true}
