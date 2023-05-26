bc68f2ca6d8e9bd75cabfcce1f7f02a6279da2a4
emidio <emidio.torre@gmail.com>
2023-05-26 10:37:58 +0200

    add full width work block

--- multiverse-web/src/app/components/WorkGrid.tsx
+++ multiverse-web/src/app/components/WorkGrid.tsx
@@ -1,22 +1,22 @@
-'use client'
-import React from 'react'
-import Card from './Card'
+"use client";
+import React from "react";
+import Card from "./Card";
 
-import Marquee from 'react-fast-marquee'
-import Container from './Container'
-import Hand from './Hand'
-import HandScrollUp from './HandScrollUp'
+import Marquee from "react-fast-marquee";
+import Container from "./Container";
+import Hand from "./Hand";
+import HandScrollUp from "./HandScrollUp";
 
 type Props = {
-  blocks: any[]
-}
+  blocks: any[];
+};
 
 function WorksGrid({ blocks }: Props) {
   //const matches = useMediaQuery('(min-width: 768px)')
   return (
     <>
       <div className="my-5">
-        <Marquee gradient={false} speed={100} style={{ overflowY: 'hidden' }}>
+        <Marquee gradient={false} speed={100} style={{ overflowY: "hidden" }}>
           <h3 className="marque-title text-8xl font-bold uppercase">
             &nbsp;discover another universe
           </h3>
@@ -31,7 +31,25 @@ function WorksGrid({ blocks }: Props) {
             .sort((a, b) => (a.item.work1.sort < b.item.work1.sort ? -1 : 1))
             .map((block: any, idx: number) => {
               switch (block.item.tipologia) {
-                case 'half-split':
+                case "full":
+                  return (
+                    <div key={idx} className="grid grid-cols-1 gap-2 mb-2">
+                      <div className="row-span-2 relative aspect-[17/5] h-full w-full">
+                        <Card
+                          status={block.item.work1?.status}
+                          aspect_ratio={block.item.work1?.aspectRatioImage}
+                          id={block.item.work1?.id}
+                          image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
+                            block.item.work1.image?.id
+                          }`}
+                          slug={block.item.work1?.slug}
+                          Name={block.item.work1?.Name}
+                          priority={idx <= 2}
+                        ></Card>
+                      </div>
+                    </div>
+                  );
+                case "half-split":
                   return (
                     <div
                       key={idx}
@@ -42,7 +60,7 @@ function WorksGrid({ blocks }: Props) {
                           status={block.item.work1?.status}
                           aspect_ratio={block.item.work1?.aspectRatioImage}
                           id={block.item.work1?.id}
-                          image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                          image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                             block.item.work1.image?.id
                           }`}
                           slug={block.item.work1?.slug}
@@ -55,7 +73,7 @@ function WorksGrid({ blocks }: Props) {
                           status={block.item.work2?.status}
                           aspect_ratio={block.item.work2?.aspectRatioImage}
                           id={block.item.work2.id}
-                          image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                          image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                             block.item.work2.image?.id
                           }`}
                           slug={block.item.work2?.slug}
@@ -69,7 +87,7 @@ function WorksGrid({ blocks }: Props) {
                           aspect_ratio={block.item.work3?.aspectRatioImage}
                           key={block.item.work3.id}
                           id={block.item.work3.id}
-                          image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                          image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                             block.item.work3.image?.id
                           }`}
                           slug={block.item.work3?.slug}
@@ -78,9 +96,9 @@ function WorksGrid({ blocks }: Props) {
                         ></Card>
                       </div>
                     </div>
-                  )
+                  );
 
-                case 'split-half':
+                case "split-half":
                   return (
                     <div
                       key={idx}
@@ -92,7 +110,7 @@ function WorksGrid({ blocks }: Props) {
                             status={block.item.work1?.status}
                             aspect_ratio={block.item.work1?.aspectRatioImage}
                             id={block.item.work1.id}
-                            image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                            image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                               block.item.work1.image?.id
                             }`}
                             slug={block.item.work1?.slug}
@@ -105,7 +123,7 @@ function WorksGrid({ blocks }: Props) {
                             status={block.item.work2?.status}
                             aspect_ratio={block.item.work2?.aspectRatioImage}
                             id={block.item.work2.image.id}
-                            image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                            image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                               block.item.work2.image?.id
                             }`}
                             slug={block.item.work2?.slug}
@@ -120,7 +138,7 @@ function WorksGrid({ blocks }: Props) {
                             status={block.item.work3?.status}
                             aspect_ratio={block.item.work3?.aspectRatioImage}
                             id={block.item.work3.image.id}
-                            image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                            image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                               block.item.work3.image?.id
                             }`}
                             slug={block.item.work3?.slug}
@@ -130,8 +148,8 @@ function WorksGrid({ blocks }: Props) {
                         </div>
                       </div>
                     </div>
-                  )
-                case 'half-half':
+                  );
+                case "half-half":
                   return (
                     <div
                       key={idx}
@@ -141,7 +159,7 @@ function WorksGrid({ blocks }: Props) {
                         <Card
                           aspect_ratio={block.item.work1?.aspectRatioImage}
                           id={block.item.work1.image.id}
-                          image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                          image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                             block.item.work1.image?.id
                           }`}
                           slug={block.item.work1?.slug}
@@ -155,7 +173,7 @@ function WorksGrid({ blocks }: Props) {
                           status={block.item.work2?.status}
                           aspect_ratio={block.item.work2?.aspectRatioImage}
                           id={block.item.work2.image.id}
-                          image_url={`${'https://multiverse-dev-directus.rizo.tech'}/assets/${
+                          image_url={`${"https://multiverse-dev-directus.rizo.tech"}/assets/${
                             block.item.work2.image?.id
                           }`}
                           slug={block.item.work2?.slug}
@@ -164,13 +182,13 @@ function WorksGrid({ blocks }: Props) {
                         ></Card>
                       </div>
                     </div>
-                  )
+                  );
               }
             })}
         <HandScrollUp />
       </Container>
     </>
-  )
+  );
 }
 
-export default WorksGrid
+export default WorksGrid;
