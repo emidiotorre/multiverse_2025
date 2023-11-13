7272efc223208c61f3aad9a67e902374e66039c9
emidio <emidio.torre@gmail.com>
2023-11-13 13:31:33 +0100

    squyare videos

--- multiverse-web/next.config.js
+++ multiverse-web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  
   images: {
     formats: ['image/webp'],
     imageSizes: [96, 128, 256, 384, 512, 640, 848, 920, 1024, 1432],
