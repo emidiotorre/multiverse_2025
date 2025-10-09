4f738f313e433597016c39b77b08c1321ded68d8
Emidio Torre <emidio.torre@gmail.com>
2025-10-09 13:26:58 +0200

    fix

--- multiverse-web/src/app/works/[slug]/page.tsx
+++ multiverse-web/src/app/works/[slug]/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { fetcher } from '@/app/apollo'
-import { GET_WORK_BY_SLUG } from '@/app/apollo/queries'
-import Project from '@/app/components/Project'
+import React from "react";
+import { fetcher } from "@/app/apollo";
+import { GET_WORK_BY_SLUG } from "@/app/apollo/queries";
+import Project from "@/app/components/Project";
 
-export default async function Work({ params }: { params: { slug: string } }) {
+export default async function Work({ params }: { params: any }) {
   //@ts-ignore
   const { work } = await fetcher({
     query: GET_WORK_BY_SLUG,
     variables: {
       slug: params.slug,
     },
-  })
+  });
 
-  return <Project work={work[0]} />
+  return <Project work={work[0]} />;
 }
